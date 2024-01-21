b975f7166ae35074d743bd2285eb6733d5fc2052
ItzAmarnath <128959336+ItzAmarnath@users.noreply.github.com>
2024-01-22 00:33:15 +0530

    Add files via upload

---
+++ Day-4 JS tasks/Resume.js
@@ -0,0 +1,49 @@
+let myResume={
+    "basic details": {
+      "name": "Amarnath P",
+      "email": "nathamar363@gmail.com",
+      "phone": 7539971031,
+      "degree": "Bsc.IT",
+      "location": {
+        "address": "Nu's Luxury service apartment, Raju nagar 1st street, Njk avenue",
+        "PinCode": 600097,
+        "city": "Chennai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "github":"https://github.com/ItzAmarnath"
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "Tata Consultancy Services",
+        "position": "Information processing specialist",
+        "startDate": "2021-01-22",
+      },
+    ],
+    "education": [
+      {
+        "institution": "The American College of Arts & science",
+        "department": "Information Technology",
+        "studyType": "fulltime",
+        "batch start year": 2017,
+        "batch end year": 2020,
+      }
+    ],
+    "skills": [
+      {
+        "name": "Html-Css,javascript",
+        "level": "beginer",
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+  }
+  console.log(myResume);
+  
